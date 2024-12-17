7c341921f443a827ab13f3f5a07e5bd8f8c949fb
Akatsuki097 <salman201103@gmail.com>
2024-12-18 00:03:19 +0600

    Add project files.

---
+++ DialPad/Program.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Application {
+    public class Pad
+    {
+
+        private static Dictionary<char, string> dialMap = new Dictionary<char, string>
+        {
+            { '1', "&'(" },
+            { '2', "abc" },
+            { '3', "def" },
+            { '4', "ghi" },
+            { '5', "jkl" },
+            { '6', "mno" },
+            { '7', "pqrs" },
+            { '8', "tuv" },
+            { '9', "wxyz" },
+            { '*', "" },
+        };
+
+        public static string OldPhonePad(string input)
+        {
+            string output = "";
+            int sameCharCount = 0;
+
+            if (input[0] != '*') 
+                sameCharCount = 1;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+
+                if (input[i] == '*')
+                {
+                    sameCharCount = 0;
+                }
+                else if (input[i] == '#' || input[i] == ' ')
+                {
+                    if (input[i - 1] == '*' || input[i - 1] == ' ') continue;
+
+                    int blockSize = dialMap[input[i - 1]].Length;
+                    int blockIdx = (sameCharCount - 1) % blockSize;
+                    char tempOutput = dialMap[input[i - 1]][blockIdx];
+
+                    output += tempOutput;
+                    sameCharCount = 1;
+
+                    if (input[i] == '#')
+                    {
+                        break;
+                    }
+                    i++;
+                }
+                else
+                {
+                    if (input[i - 1] == '*')
+                    {
+                        sameCharCount = 1;
+                    }
+                    else if (input[i] == input[i - 1])
+                    {
+                        sameCharCount++;
+                    }
+                    else
+                    {
+                        int blockSize = dialMap[input[i - 1]].Length;
+                        int blockIdx = (sameCharCount - 1) % blockSize;
+                        char tempOutput = dialMap[input[i - 1]][blockIdx];
+
+                        output += tempOutput;
+                        sameCharCount = 1;
+                           
+                    }
+
+                }
+            }
+            return output;
+        }
+    }
+
+}
+
+
---
+++ DialPadTests/PadTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Tests
+{
+    [TestClass()]
+    public class PadTests
+    {
+        [TestMethod()]
+        public void OldPhonePadTest()
+        {
+            Assert.AreEqual(Pad.OldPhonePad("8 88777444666*664#"), "turing");
+        }
+    }
+}
